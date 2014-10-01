a32d9f2b59cbbae01637b0a79c84ad35d74b140f
Philippe Latulippe <philippe@zone42.ca>
2014-10-01 18:15:56 -0400

    This aproach does not work, as the API can result any type of result If I query for the city list and there is only one branch in that country, it will return a branch!

--- deutschebank_scrape.py
+++ deutschebank_scrape.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 import urllib.request
 import time
 
+import sqlite3
+
 url_countries = "https://secure.deutsche-bank.de/cc/locationsfinder/en/xml.do?type=countries&_="
 
 url_cities = "https://secure.deutsche-bank.de/cc/locationsfinder/en/xml.do?type=cities&country=%s&_="
@@ -14,8 +16,22 @@ url_branch ="https://secure.deutsche-bank.de/cc/locationsfinder/en/xml.do?type=d
 
 
 
+debugging = True
+def info(*args, **blargs):
+    if debugging:
+        print(*args, **blargs)
+
+
+
+sql = sqlite3.connect('deutschebank.sqlite3')
+cursor = sql.cursor()
+sql.execute("CREATE TABLE IF NOT EXISTS BranchInfo(id TEXT, detail TEXT, "
+            "value TEXT)")
+
 
 def scrape_branch_info(branch_id):
+    info("Scraping branch ID " + branch_id)
+
     req_branch = urllib.request.urlopen(url_branch % branch_id)
     data_branch = req_branch.read().decode('utf-8')
     xml_branch = BeautifulSoup(data_branch,features="xml")
@@ -27,38 +43,50 @@ def scrape_branch_info(branch_id):
         table_branch = html_branch.find("table")
         rows_branch = table_branch.find_all("tr", recursive=False)
 
-        print("(")
+        info("(")
+        collecting_data = False
         for row in rows_branch:
             cols = row.find_all("td")
             left = cols[0]
             right = cols[1]
 
+            #don't collect data for specialized offices
+            if collecting_data and left.find(attrs={"class":"table_hl"}):
+                break
+
             left_text = ""
             for string in left.stripped_strings:
-                left_text = left_text+ string.replace("\u00A0"," "),end=""
-                print(left_text)
+                left_text = left_text+ string.replace("\u00A0"," ")
+                info(left_text, end="")
 
-            print("---->",end="")
+            info("---->",end="")
     
             right_text = ""
-            for string in right.stripped_strings:
-                right_text = right_text + string.replace("\u00A0"," "))
-                print(right_text)
+            right_table = right.find("table")
+            if right_table:
+                right_text = right.decode_contents()
+            else:
+                for string in right.stripped_strings:
+                    right_text = right_text + string.replace("\u00A0"," ") + "\n"
 
-            #Some branches have multiple offices and diff info for them, we
-            #need to stop scraping when we find another tag with class=table_hl
+            info(right_text)
 
-            print()
-        print(")")
+            if len(right_text) > 0 and len(left_text) > 0:
+                collecting_data = True
+                cursor.execute("INSERT INTO BranchInfo VALUES(?,?,?)",
+                                (branch_id, left_text, right_text));
+
+            info()
+        info(")")
 
             
 
-    
 
-
-#complex example
-scrape_branch_info("73222")
-exit()
+##complex example
+#scrape_branch_info("73222")
+#sql.commit()
+#sql.close()    
+#exit()
 
 req_countries = urllib.request.urlopen(url_countries)
 data_countries = req_countries.read().decode('utf-8')
@@ -69,7 +97,7 @@ country_ids = xml_countries.find_all("value")
 for country_node in country_ids:
     country_id = country_node.getText()
     
-    print(country_id)
+    info("Country: "+country_id)
 
     req_cities = urllib.request.urlopen(url_cities % country_id)
     data_cities = req_cities.read().decode('utf-8')
@@ -80,7 +108,7 @@ for country_node in country_ids:
     for city_node in city_ids:
         city_id = city_node.getText()
 
-        print("-> "+city_id)
+        info("-> city "+city_id)
 
         req_branches = urllib.request.urlopen(url_branches % (country_id, 
                                               city_id))
@@ -91,7 +119,12 @@ for country_node in country_ids:
         
         for branch_node in branches_ids:
             branch_id = branch_node.getText()
-            print("branch: "+branch_id)
+            info("branch: "+branch_id)
             scrape_branch_info(branch_id)
+            time.sleep(0.9)
+            
+        time.sleep(0.4)
 
-        time.sleep(2)
+
+
+    time.sleep(0.4)
